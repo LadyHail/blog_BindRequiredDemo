2f66c55cc21eaffd62f62ebce4fc924b4acfb649
LadyHail <ladydeath@o2.pl>
2020-06-02 20:41:13 +0200

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using BindRequiredAttributeDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BindRequiredAttributeDemo.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [Route("/")]
+        public IActionResult Create([FromQuery]Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(product);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace BindRequiredAttributeDemo.Models
+{
+    public class Product
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [BindRequired]
+        public decimal Price { get; set; }
+    }
+}
